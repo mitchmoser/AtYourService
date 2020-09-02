3822f8800cdbdfb926edf263ee928a84e7d03d43
Mitchell Moser <moser.mitch@yahoo.com>
2020-09-02 09:27:30 -0500

    Add files via upload
    Added multithreading

--- AtYourService/Program.cs
+++ AtYourService/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Management;
+using System.Threading;
 
 namespace AtYourService
 {
@@ -18,53 +19,56 @@ namespace AtYourService
             string ns = @"root\cimv2";
             foreach (string host in hosts)
             {
-                ManagementScope scope = new ManagementScope(string.Format(@"\\{0}\{1}", host, ns));
-                //https://docs.microsoft.com/en-us/windows/win32/wmisdk/wql-operators
-                //https://docs.microsoft.com/en-us/windows/win32/cimwin32prov/win32-service
-                SelectQuery query = new SelectQuery("SELECT name,displayname,startname,description,systemname FROM Win32_Service WHERE startname IS NOT NULL");
+                Thread newThread = new Thread(() => GetServices(ns, host));
+                newThread.Start();
+            }
+        }
+        
+        static void GetServices(string ns, string host)
+        {
+            ManagementScope scope = new ManagementScope(string.Format(@"\\{0}\{1}", host, ns));
+            //https://docs.microsoft.com/en-us/windows/win32/wmisdk/wql-operators
+            //https://docs.microsoft.com/en-us/windows/win32/cimwin32prov/win32-service
+            SelectQuery query = new SelectQuery("SELECT name,displayname,startname,description,systemname FROM Win32_Service WHERE startname IS NOT NULL");
 
-                // Requires Local Admin. Try catch for insufficient privs, network connectivity issues, etc
-                try
+            // Requires Local Admin. Try catch for insufficient privs, network connectivity issues, etc
+            try
+            {
+                Console.WriteLine("[+] Connecting to {0}", host);
+                scope.Connect();
+                //https://stackoverflow.com/questions/842533/in-c-sharp-how-do-i-query-the-list-of-running-services-on-a-windows-server
+                using (ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query))
                 {
-                    Console.WriteLine("[+] Connecting to {0}", host);
-                    scope.Connect();
-                    //https://stackoverflow.com/questions/842533/in-c-sharp-how-do-i-query-the-list-of-running-services-on-a-windows-server
-                    using (ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query))
+                    Console.WriteLine("[+] {0} - Enumerating services", host);
+                    ManagementObjectCollection services = searcher.Get();
+                    Console.WriteLine("[+] {0} - Found {1} services running", host, services.Count);
+                    Console.WriteLine("[+] {0} - Filtering out LocalSystem and NT Authority Account services", host);
+                    bool results = false;
+                    foreach (ManagementObject service in services)
                     {
-                        Console.WriteLine("[+] Enumerating services...");
-                        ManagementObjectCollection services = searcher.Get();
-                        Console.WriteLine("[+] Found {0} services running...", services.Count);
-                        Console.WriteLine("[+] Filtering out LocalSystem and NT Authority Account services...");
-                        bool results = false;
-                        foreach (ManagementObject service in services)
+                        // Exclude services running as local accounts
+                        string[] exclusions = { "LOCALSYSTEM", "NT AUTHORITY\\LOCALSERVICE", "NT AUTHORITY\\NETWORKSERVICE" };
+                        if (!exclusions.Contains(service["StartName"].ToString().ToUpper()))
                         {
-                            // Exclude services running as local accounts
-                            string[] exclusions = { "LOCALSYSTEM", "NT AUTHORITY\\LOCALSERVICE", "NT AUTHORITY\\NETWORKSERVICE" };
-                            if (!exclusions.Contains(service["StartName"].ToString().ToUpper()))
-                            {
-                                Console.WriteLine("\t[+] Service:\t {0}", service["Name"]);
-                                Console.WriteLine("\t    Name:\t {0}", service["DisplayName"]);
-                                Console.WriteLine("\t    Account:\t {0}", service["StartName"]);
-                                Console.WriteLine("\t    Description: {0}", service["Description"]);
-                                Console.WriteLine("\t    System:\t {0}", service["SystemName"]);
-                                results = true;
-                            }
-                        }
-                        if (!results)
-                        {
-                            Console.WriteLine("[!] No other services identified on {0}", host);
-                            continue;
+                            Console.WriteLine("[+] Host: {0}", service["SystemName"]);
+                            Console.WriteLine(" Account: {0}", service["StartName"]);
+                            Console.WriteLine(" Service: {0}", service["Name"]);
+                            Console.WriteLine("    Name: {0}", service["DisplayName"]);
+                            Console.WriteLine("    Info: {0}", service["Description"]);
+                            results = true;
                         }
                     }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("[!] " + ex.Message);
-                    Console.WriteLine("[!] Unable to query services on {0}", host);
-                    continue;
+                    if (!results)
+                    {
+                        Console.WriteLine("[!] {0} - No other services identified", host);
+                    }
                 }
             }
-            Console.WriteLine("[+] Finished");
+            catch (Exception ex)
+            {
+                Console.WriteLine("[!] " + ex.Message);
+                Console.WriteLine("[!] {0} - Unable to query services", host);
+            }
         }
     }
 }
